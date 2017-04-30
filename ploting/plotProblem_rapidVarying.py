d0be6dac1dcdd4dd1dfdfd9ddbd44997c1ddcb4c
costa <costadvl@gmail.com>
2017-05-01 00:20:19 +0200

    redefining zero devision posible error

--- ploting/plotProblem_rapidVarying.py
+++ ploting/plotProblem_rapidVarying.py
@@ -2,9 +2,11 @@ import numpy as np
 import matplotlib.pylab as pl
 
 def f(x):
-    return np.sin(1.0/x)
+    r = x.copy()
+    r[x==0] = np.exp(1-100)
+    return np.sin(1.0/r)
 
-x1 = np.linspace(-1,1,10)   # f(0) sin(1/0) caution! should be excluded
+x1 = np.linspace(-3,3,71)   # f(0) sin(1/0) caution! should be excluded
 x2 = np.linspace(-1,1,1000)
 pl.subplot(2,1,1)
 pl.plot(x1,f(x1))
